6e72ae8c9a0884015932c90875229f17bbeb23f5
baranheidari <127683387+baranheidari@users.noreply.github.com>
2025-11-20 12:14:19 -0500

    Create security group for SSH and app access
    This security group allows SSH and application traffic.

---
+++ terraform/security-group.tf
@@ -0,0 +1,28 @@
+resource "aws_security_group" "ec2_sg" {
+  name        = "clo835-assignment1-sg"
+  description = "Allow SSH and app ports"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "App ports"
+    from_port   = 8081
+    to_port     = 8083
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
